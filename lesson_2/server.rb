65baf51bd1a44ed9a590cb85ae5f854c535b448c
Madz No <madzno12@gmail.com>
2022-09-19 14:53:57 -0400

    updating parse request method

--- lesson_2/server.rb
+++ lesson_2/server.rb
@@ -1,12 +1,36 @@
 require "socket"
 
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  parameters = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [http_method, path, parameters]
+end
+
 server = TCPServer.new("localhost", 3003)
 loop do
   client = server.accept
 
   request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
   puts request_line
 
+  http_method, path, parameters = parse_request(request_line)
+
   client.puts "HTTP/1.1 200 OK\r\n\r\n"
+
+  rolls = parameters["rolls"].to_i
+  sides = parameters["sides"].to_i
+
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts roll
+  end
+
   client.close
 end
