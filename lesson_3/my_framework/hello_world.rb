48ecc1943d34365dcbeed2378d8c90990bf79040
Madz No <madzno12@gmail.com>
2022-09-22 16:17:14 -0400

    updating framework routes

--- lesson_3/my_framework/hello_world.rb
+++ lesson_3/my_framework/hello_world.rb
@@ -1,5 +1,29 @@
+require_relative 'advice'
+
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain' }, ['Hello World!']]
+    case env['REQUEST_PATH']
+    when '/'
+      [
+        '200',
+        {'Content-Type' => 'text/html' },
+        ['<html><body><h2>Hello World!</h2></body></html>']
+      ]
+    when '/advice'
+      piece_of_advice = Advice.new.generate
+      [
+        '200',
+        {"Content-Type" => 'text/html'},
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
+    else
+      [
+        '404',
+        {"Content-Type" => 'text/html', "Content-Length" => '48'},
+        ["<html><body><h4>404 Not Found</h4></body></html>"]
+      ]
+    end
   end
 end
+
+
